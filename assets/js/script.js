f440ce04bae37dcb6e529b9d5e89a641c40d38e7
Zzaaf <zzaafamania@gmail.com>
2019-09-13 00:15:43 +0300

    New layout, conditions for checkbox added, template field added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,24 @@ let dataFrom = document.getElementById('dateFrom'),
     timeTo = document.getElementById('timeTo'),
 
     dateTime = document.getElementById('dateTime'),
-    dateTimeValue,    
+    dateTimeCont = document.getElementById('dateTimeCont'),
+    dateTimeValue,
 
     ad = document.getElementById('ad'),
-    adVal = document.getElementById('adVal'),
+    adCont = document.getElementById('adCont'),
+    adValue,
+
+    profile = document.getElementById('profile'),
+    profileCont = document.getElementById('profileCont'),
+    profileValue,
+
+    banner = document.getElementById('banner'),
+    bannerCont = document.getElementById('bannerCont'),
+    bannerValue,
+
+    typeNum = document.getElementById('typeNum'),
+    typeNumCont = document.getElementById('typeNumCont'),
+    typeNumValue,
 
     result = document.getElementById('result'),
     copyCommand = document.getElementById('copyCommand'),
@@ -42,18 +56,47 @@ clearCommand.onclick = () => {
 getCommand.onclick = () => {
   let dateTimeRange = `${dataFrom.value}T${timeFrom.value} ${dataTo.value}T${timeTo.value}`;
 
-  // if (ad.checked == true && adVal.value != '') {
-  //   adVal.value = `ad=${adVal.value}`;
-  // }
-
-  if (dateTime.checked == true) {
-    dateTimeValue = 'datetime,';
+  if (dateTime.checked == true && dateTimeCont.value != '') {
+    dateTimeValue = `datetime=${dateTimeCont.value}`;
+  } else if (dateTime.checked == true) {    
+    dateTimeValue = 'datetime';
   } else {
     dateTimeValue = '';
   }
 
+  if (ad.checked == true && adCont.value != '') {
+    adValue = `,ad=${adCont.value}`;
+  } else if (ad.checked == true) {
+    adValue = ',ad';
+  } else {
+    adValue = '';
+  }
+
+  if (profile.checked == true && profileCont.value != '') {
+    profileValue = `,profile=${profileCont.value}`;
+  } else if (profile.checked == true) {
+    profileValue = ',profile';
+  } else {
+    profileValue = '';
+  }
+
+  if (banner.checked == true && bannerCont.value != '') {
+    bannerValue = `,bid=${bannerCont.value}`;
+  } else if (banner.checked == true) {
+    bannerValue = ',bid';
+  } else {
+    bannerValue = '';
+  }
+
+  if (typeNum.checked == true && typeNumCont.value != '') {
+    typeNumValue = `,typenum=${typeNumCont.value}`;
+  } else if (typeNum.checked == true) {
+    typeNumValue = ',typenum';
+  } else {
+    typeNumValue = '';
+  }
 
   if (dataFrom.value != '' && dataTo.value != '') {
-    result.value = `history_log -P ${dateTimeRange} "${dateTimeValue}${adVal.value}"`;
+    result.value = `history_log -P ${dateTimeRange} "${dateTimeValue}${adValue}${profileValue}${bannerValue}${typeNumValue}"`;
   }    
 }
